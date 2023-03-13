706653c70b5973af08952b986794d9a67f87559a
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2023-03-13 18:36:19 -0400

    Changed motors on drivetrain to brake instead of cost when idle

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -26,25 +26,25 @@ public class DrivetrainSubsystem extends SubsystemBase {
         m_frontLeftMotor  = new CANSparkMax(Constants.Drivetrain.kFrontLeftCanId, CANSparkMaxLowLevel.MotorType.kBrushed);
         m_frontLeftMotor.setInverted(Constants.Drivetrain.kFrontLeftInverted);
         m_frontLeftMotor.setSmartCurrentLimit(Constants.Drivetrain.kCurrentLimit);
-        m_frontLeftMotor.setIdleMode(IdleMode.kCoast);
+        m_frontLeftMotor.setIdleMode(IdleMode.kBrake);
         m_frontLeftMotor.burnFlash();
 
         m_frontRightMotor = new CANSparkMax(Constants.Drivetrain.kFrontRightCanId, CANSparkMaxLowLevel.MotorType.kBrushed);
         m_frontRightMotor.setInverted(Constants.Drivetrain.kFrontRightInverted);
         m_frontRightMotor.setSmartCurrentLimit(Constants.Drivetrain.kCurrentLimit);
-        m_frontRightMotor.setIdleMode(IdleMode.kCoast);
+        m_frontRightMotor.setIdleMode(IdleMode.kBrake);
         m_frontRightMotor.burnFlash();
 
         m_rearLeftMotor   = new CANSparkMax(Constants.Drivetrain.kRearLeftCanId, CANSparkMaxLowLevel.MotorType.kBrushed);
         m_rearLeftMotor.setInverted(Constants.Drivetrain.kRearLeftInverted);
         m_rearLeftMotor.setSmartCurrentLimit(Constants.Drivetrain.kCurrentLimit);
-        m_rearLeftMotor.setIdleMode(IdleMode.kCoast);
+        m_rearLeftMotor.setIdleMode(IdleMode.kBrake);
         m_rearLeftMotor.burnFlash();
 
         m_rearRightMotor  = new CANSparkMax(Constants.Drivetrain.kRearRightCanId, CANSparkMaxLowLevel.MotorType.kBrushed);
         m_rearRightMotor.setInverted(Constants.Drivetrain.kRearRightInverted);
         m_rearRightMotor.setSmartCurrentLimit(Constants.Drivetrain.kCurrentLimit);
-        m_rearRightMotor.setIdleMode(IdleMode.kCoast);
+        m_rearRightMotor.setIdleMode(IdleMode.kBrake);
         m_rearRightMotor.burnFlash();
     }
 
